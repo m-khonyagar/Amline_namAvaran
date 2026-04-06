b4229dec779a990a2a92853606c7afbf4696ad55
Mohammad Khonyagar <m.khonyagar1@gmail.com>
2026-04-06 05:47:29 +0330

    fix: add SMS OTP service (Kavenegar), httpx dep, production auth flow

--- consultant-ui/src/mocks/browser.ts
+++
@@ -1,4 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { consultantSelfServiceHandlers } from '../../../admin-ui/src/mocks/consultantPlatformHandlers';
-
-export const worker = setupWorker(...consultantSelfServiceHandlers());
