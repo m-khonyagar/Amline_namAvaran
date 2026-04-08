24a81cbb94c6b2731bf2f1a0639553a8a61055d2
m-khonyagar <158306758+m-khonyagar@users.noreply.github.com>
2026-04-08 07:00:00 +0330

    fix(consultant-ui): import handlers from local mocks

--- consultant-ui/src/mocks/browser.ts
+++ consultant-ui/src/mocks/browser.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from 'msw/browser';
-import { consultantSelfServiceHandlers } from '../../../admin-ui/src/mocks/consultantPlatformHandlers';
+import { consultantSelfServiceHandlers } from './consultantPlatformHandlers';
 
 export const worker = setupWorker(...consultantSelfServiceHandlers());
